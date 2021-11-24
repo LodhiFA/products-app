715a4e4661c691ea7e0c4d3e2db58f03df00ce08
LodhiFA <f.lodhi22@gmail.com>
2021-11-25 02:50:00 +0500

    - Added search component to filter products

---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,41 @@
+import React, { ChangeEvent, useState, useEffect } from 'react'
+import { Form, Card } from 'react-bootstrap'
+import { useAppDispatch } from '../../app/hooks'
+import { searchProducts } from '../../slices/product/productSlice'
+
+export const Search = () => {
+  const dispatch = useAppDispatch()
+
+  const [search, setSearch] = useState('')
+
+  const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value)
+  }
+
+  useEffect(() => {
+    dispatch(searchProducts(search))
+  }, [search, dispatch])
+
+  return (
+    <Card>
+      <Card.Body>
+        <Card.Title>Search Products</Card.Title>
+        <Card.Text as='div'>
+          <Form>
+            <Form.Group className='mb-3' controlId='formBasicSearch'>
+              <Form.Control
+                type='text'
+                placeholder='Search Products'
+                value={search}
+                onChange={handleSearch}
+              />
+              <Form.Text className='text-muted'>
+                Search the products by title.
+              </Form.Text>
+            </Form.Group>
+          </Form>
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  )
+}
