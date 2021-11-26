3318a64f106473eb9783f986fb69990559f00a78
LodhiFA <f.lodhi22@gmail.com>
2021-11-26 08:24:38 +0500

    - Moved index and bootstrap css files - Updated references

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import App from './App'
 import { store } from './redux/app/store'
 import { Provider } from 'react-redux'
 import * as serviceWorker from './serviceWorker'
+
+import './assets/styles/bootstrap.min.css'
+import './assets/styles/index.css'
 
 ReactDOM.render(
   <React.StrictMode>
