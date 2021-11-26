794024616f2ce3f070d13e06608632d3c71aad45
LodhiFA <f.lodhi22@gmail.com>
2021-11-26 08:23:40 +0500

    - Created wrapper to configure redux store for component documentation - Config added to styleguide.config for using wrapper - Script added for running documentation on dev server

---
+++ src/styleguide/Wrapper.js
@@ -0,0 +1,21 @@
+// src/styleguide/Wrapper.js
+import React, { Component } from 'react'
+import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
+import productReducer from '../redux/slices/product/productSlice'
+import modalReducer from '../redux/slices/modal/modalSlice'
+import initialState from '../tests/testData'
+
+const store = configureStore({
+  reducer: {
+    product: productReducer,
+    modal: modalReducer,
+  },
+  preloadedState: initialState,
+})
+
+export default class Wrapper extends Component {
+  render() {
+    return <Provider store={store}>{this.props.children}</Provider>
+  }
+}
